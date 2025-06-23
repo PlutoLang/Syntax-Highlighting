a60c641c955b10209c4c0c3898092270bb2d9598
Sainan <63328889+Sainan@users.noreply.github.com>
2025-06-23 04:49:51 +0200

    diff reduction

--- test.js
+++ test.js
@@ -62,16 +62,15 @@ async function main()
     };
 
     checkClassification(
-        `local enums = {}`,
-        `-----            storage.modifier.pluto
+`local enums = {}`,
+`-----            storage.modifier.pluto
             -    keyword.operator.assignment.pluto
               -  punctuation.section.table.begin.pluto
                - punctuation.section.table.end.pluto
 `);
-
-    checkClassification(
-        `local a = || -> 1`,
-        `-----             storage.modifier.pluto
+checkClassification(
+`local a = || -> 1`,
+`-----             storage.modifier.pluto
       -           entity.name.function.arrow.pluto
         -         keyword.operator.assignment.pluto
           -       punctuation.section.group.begin.pluto
