6396ebe8541d986dc3a2c06a4bef77f699d8c30c
Sainan <63328889+Sainan@users.noreply.github.com>
2025-06-23 04:24:48 +0200

    Lay the groundwork for automated testing

---
+++ test.js
@@ -0,0 +1,78 @@
+const fs = require("fs");
+const path = require("path");
+const vsctm = require("vscode-textmate");
+const onig = require("vscode-oniguruma");
+
+async function loadGrammar()
+{
+    const wasmPath = require.resolve("vscode-oniguruma/release/onig.wasm");
+    const wasm = fs.readFileSync(wasmPath).buffer;
+    await onig.loadWASM(wasm);
+    const onigLib = {
+        createOnigScanner: (sources) => new onig.OnigScanner(sources),
+        createOnigString: (str) => new onig.OnigString(str)
+    };
+
+    const registry = new vsctm.Registry({
+        onigLib,
+        loadGrammar: function (scopeName)
+        {
+            if (scopeName == "source.pluto")
+            {
+                const tmLanguage = fs.readFileSync(path.join(__dirname, "Pluto.tmbundle", "Syntaxes", "Pluto.tmLanguage"), "utf8");
+                return vsctm.parseRawGrammar(tmLanguage);
+            }
+            return null;
+        }
+    });
+
+    return registry.loadGrammar("source.pluto");
+}
+
+function createClassificationString(grammar, code)
+{
+    const { tokens } = grammar.tokenizeLine(code);
+    const res = [];
+    for (const token of tokens)
+    {
+        if (token.scopes.length > 1)
+        {
+            res.push(" ".repeat(token.startIndex));
+            res.push("-".repeat(token.endIndex - token.startIndex));
+            res.push(" ".repeat(1 + code.length - token.endIndex));
+            res.push(token.scopes[token.scopes.length - 1]);
+            res.push("\n");
+        }
+    }
+    return res.join("");
+}
+
+async function main()
+{
+    const grammar = await loadGrammar();
+    let ok = true;
+    const checkClassification = function(code, expected)
+    {
+        const actual = createClassificationString(grammar, code);
+        if (actual != expected)
+        {
+            console.log(`Mismatch for ${code}`);
+            ok = false;
+        }
+    };
+
+    checkClassification(
+`local enums = {}`,
+`-----            storage.modifier.pluto
+            -    keyword.operator.assignment.pluto
+              -  punctuation.section.table.begin.pluto
+               - punctuation.section.table.end.pluto
+`);
+
+    if (!ok)
+    {
+        process.exit(1);
+    }
+}
+
+main();
