e4e49af657c50e991ca33d6d3ed502409fd7aff5
Sainan <63328889+Sainan@users.noreply.github.com>
2025-08-05 21:11:08 +0200

    Handle indentation for try-catch syntax (#28)

--- test.js
+++ test.js
@@ -101,6 +101,12 @@ async function main()
     checkIndentation("function foo()", true, false);
     checkIndentation("if cond then", true, false);
     checkIndentation("else", true, true);
+    checkIndentation("try", true, false);
+    checkIndentation("pluto_try", true, false);
+    checkIndentation("catch e then", true, true);
+    checkIndentation("catch e do", true, true);
+    checkIndentation("pluto_catch e then", true, true);
+    checkIndentation("pluto_catch e do", true, true);
     checkIndentation("end", false, true);
     checkIndentation("until finished", false, true);
     checkIndentation("values = {", true, false);
